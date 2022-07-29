32651c78f108f995540cf72b2818a26e8c7835c9
Aimeri Baddouh <aimeri@baddouh.me>
2022-07-29 01:55:25 -0400

    allow json api routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  # In production I would protect the JSON api behind a token based AuthenticatedController
+  protect_from_forgery unless: -> { request.format.json? }
 end
