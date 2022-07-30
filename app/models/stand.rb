8bad12de275459e8db27f52496e3d4828d2431b1
Aimeri Baddouh <aimeri@baddouh.me>
2022-07-29 22:46:52 -0400

    add model validation

--- app/models/stand.rb
+++ app/models/stand.rb
@@ -4,4 +4,9 @@ class Stand < ApplicationRecord
                    :distance_field_name => :distance,
                    :lat_column_name => :latitude,
                    :lng_column_name => :longitude
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :object_id, presence: true
+  validates :latitude, presence: true, numericality: true
+  validates :longitude, presence: true, numericality: true
 end
